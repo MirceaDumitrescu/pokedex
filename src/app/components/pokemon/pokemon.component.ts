38d0c690625894414afe27b05da2e336d42934a9
Mircea Dumitrescu <mirceagab@gmail.com>
2023-03-06 15:43:35 +0800

    first attempt with rxjs

--- src/app/components/pokemon/pokemon.component.ts
+++ src/app/components/pokemon/pokemon.component.ts
@@ -1,27 +1,53 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, SimpleChanges } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { ApiService } from "../../api/api.service";
 import { Pokemon } from "../../api/api.service";
 import { FavoritePokemonAction } from "src/app/store/models/actions/pokemons.actions";
 import { Store } from "@ngrx/store";
+import { StateService } from "src/app/services/state.service";
+import { Observable } from "rxjs";
+
+interface FavoritePokemons {
+	favorites: Pokemon[];
+}
+
+const initialState: FavoritePokemons = { favorites: [] };
 
 @Component({
 	selector: "app-pokemon",
 	templateUrl: "./pokemon.component.html",
 	styleUrls: ["./pokemon.component.scss"],
 })
-export class PokemonComponent {
-	public pokemon: Pokemon | null = null;
+export class PokemonComponent extends StateService<FavoritePokemons> implements OnInit {
+	public pokemon: any;
 	public id: number = 0;
 	public loading: boolean = true;
 	public isFavorite: boolean = false;
+	public isFavoriteRxJs: boolean = false;
 	public favorites: Pokemon[] = [];
+	public favoritesRXJS: Pokemon[] = [];
 
+	favoritesRxjs$: Observable<Pokemon[]> = this.select((state) => state.favorites);
+
+	addToFavoritesRxjs(pokemon: Pokemon): void {
+		this.setState({ favorites: [...this.favoritesRXJS, pokemon] });
+	}
+
+	removeFromFavoritesRxjs(pokemon: Pokemon): void {
+		this.setState({ favorites: this.favoritesRXJS.filter((favorite: Pokemon) => favorite.id !== pokemon.id) });
+	}
+
+	ngOnInit(): void {}
+
+	ngOnChanges(changes: SimpleChanges) {
+		console.log(changes);
+	}
 	constructor(
 		private route: ActivatedRoute,
 		private apiService: ApiService,
 		private store: Store<{ favorites: Pokemon[] }>
 	) {
+		super(initialState);
 		this.id = this.route.snapshot.params["id"];
 
 		this.apiService.getPokemon(this.id).subscribe((pokemon: Pokemon) => {
@@ -32,6 +58,14 @@ export class PokemonComponent {
 		this.store.select("favorites").subscribe((favorites) => {
 			this.favorites = favorites;
 			this.isFavorite = this.favorites.some((favorite: Pokemon) => favorite.id === this.id);
+		});
+
+		this.favoritesRxjs$.subscribe((state: Pokemon[]) => {
+			this.favoritesRXJS = state;
+			console.log("favorites onInitState", state);
+			this.isFavoriteRxJs = state.some((favorite: Pokemon) => favorite.id === this.id);
+			console.log("id", this.id);
+			console.log("isFavoriteRxJs", this.isFavoriteRxJs);
 		});
 	}
 
